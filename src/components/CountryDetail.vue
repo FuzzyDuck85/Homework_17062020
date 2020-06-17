3ec26259035ea0c8a53a6feba13a573f6899a759
Donald Cameron <drc85@live.co.uk>
2020-06-17 16:53:01 +0100

    MVP completed and flags inserted working on drop down list

---
+++ src/components/CountryDetail.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <div v-if="country">
+    <h3>{{country.name}}</h3>
+    <p>Population: {{country.population}}</p>
+    <p>Region:{{country.region}}</p>
+    <p v-for="(country, index) in countries">Languages Spoken: {{country.languages.name}}</p>
+    <img class="med-flag" :src="country.flag"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'country-detail',
+  props: ['country']
+}
+</script>
+
+<style lang="css" scoped>
+.med-flag {
+  height: 40px
+}
+</style>
---
+++ src/components/CountrySelector.vue
@@ -0,0 +1,31 @@
+<template lang="html">
+  <div class="">
+    <select v-model="selectedCountry" v-on:change="handleChange">
+      <option v-for="(country, index) in countries" :country="country" :key="index"></option>
+    </select>
+  </div>
+</template>
+
+<script>
+import {eventBus} from '../main.js'
+export default {
+  name: 'countries-list',
+  props: ['countries'],
+  components: {
+    "list-item": ListItem
+  },
+  data(){
+    return{
+      selectedCountry: null
+    }
+  },
+  methods: {
+    handleChange(){
+      eventBus$emit('country-select', this.selectedCountry)
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
